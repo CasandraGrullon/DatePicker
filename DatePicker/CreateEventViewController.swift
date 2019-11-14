15abff17e86e66edee55ab53e1266388474f37f7
Casandra <casandragrullon@pursuit.org>
2019-11-14 13:02:14 -0500

    formatted date. update event name and date in our segue

--- DatePicker/CreateEventViewController.swift
+++ DatePicker/CreateEventViewController.swift
@@ -35,14 +35,32 @@ class CreateEventViewController: UIViewController {
         //Date() creates/instantiates a new Date object with the current date and time.
         event = Event(name: "event name not set", willAttend: false, date: Date())
         
+        
+        //configure datePicker
+        eventDatePicker.minimumDate = Date()
+        
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         print("prepare for segue")
         //here is where we set up and pass event data to the next vc
+        
+        //1.we need an instance of the second vc
+        //segue.source => where segue comes from
+        //segue.destination is where the segue is going
+        
+        //need to type cast because the parent class doesnt know about the subclass
+        guard let detailViewController = segue.destination as? DetailViewController else {
+            return
+        }
+        //now we could set the event on the second vc
+        detailViewController.event = event
     }
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        event.date = sender.date
+        //updating the event's date
+        
     }
     
 
@@ -52,6 +70,8 @@ extension CreateEventViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         eventTextField.resignFirstResponder()
+        //updating event's name
+        event.name = eventTextField.text ?? ""
         return true
     }
 }
--- DatePicker/DetailViewController.swift
+++ DatePicker/DetailViewController.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var selectedDateLabel: UILabel!
+    @IBOutlet weak var switchControl: UISwitch!
+    
     //we need object(Event) from source viewcontroller => create event view controller
     var event : Event? // default value is nill
-    
+
+    //DateFormatter will help us format the Date object in a more readable format
+    // lazy means we only use this variable when its needed.
+    // gets created only the first time its needed
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        
+        return formatter
+    }() //calls this function (closure)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         
     }
 
+    func updateUI() {
+        guard let date = event?.date else {
+            return
+        }
+        
+        selectedDateLabel.text = dateFormatter.string(from: date)
+        
+        //set switch position based on .willAttend == true or false.
+        switchControl.isOn = event?.willAttend ?? false
+        messageLabel.text = event?.name ?? "Event name not available."
+    }
 
+    @IBAction func rsvpChanged(_ sender: UISwitch) {
+        
+    }
+    
 }
